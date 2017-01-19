090b5db0b5ea766ec54f4338e398a402a8ed4608
Juan Crisóstomo <juancvfenix@gmail.com>
2017-01-19 17:12:29 -0600

    yeahgit add ., conexión por ws a mqtt

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require ("express");
+var express = require("express");
 
 var app = express(),
     port = 3010;
 
 
-/*
+
 app.use(express.static("public"));
 
 
-app.listen(port,  function (err) {
+app.listen(port, function (err) {
 
     if (err) {
 
@@ -17,25 +17,10 @@ app.listen(port,  function (err) {
     } else {
 
         var message = "SE INICIÓ EL SERVIDOR EN EL PUERTO: " + port;
-    
+
         console.log(message);
 
     }
 
 });
 
-*/
-
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://activemq3870.cloudapp.net')
- 
-client.on('connect', function () {
-  client.subscribe('mqtt/demo')
-  client.publish('mqtt/demo', 'holaaaaa')
-})
- 
-client.on('message', function (topic, message) {
-  // message is Buffer 
-  console.log(message.toString())
-//  client.end()
-})
